a95fba4a1ab00226a09c2b74e7b1ccdc8ea7111b
Samarthpatil1234 <samarthpatil2047@gmail.com>
2024-06-26 14:29:11 +0530

    Add files via upload

---
+++ 2b.py
@@ -0,0 +1,10 @@
+a=int(input("enter a decimal no:"))
+def dectobin(a):
+    print("binary=",bin(a))
+dectobin(a)
+def dectohex(a):
+    print("hexadecimal=",hex(a))
+dectohex(a)
+def dectooct(a):
+    print("octal=",oct(a))
+dectooct(a)
